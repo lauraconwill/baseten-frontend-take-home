feb6a2d1d33e10853baf8533f0c50c4bf2fb4978
Laura Conwill <lauraconwill@gmail.com>
2023-10-04 13:48:17 -0700

    host logo image locally

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,11 @@
 import { FunctionComponent } from "react";
+import { ReactComponent as Logo } from "./assets/Baseten_Logo_Primary_Symbol.svg";
 
 const Header: FunctionComponent = () => {
   return (
     <div className="Header">
       <div className="Logo">
-        <img
-          height="30"
-          alt="Baseten logo"
-          src="https://app.baseten.co/static/Baseten_Logo_Primary_Symbol.svg"
-        />
+        <Logo height="30px" />
       </div>
       <div className="Text">ML Model Search</div>
     </div>
